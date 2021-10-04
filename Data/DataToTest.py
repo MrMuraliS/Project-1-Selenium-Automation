d26ce40fb77fe9422c01c6a44cc71c337410d0a4
MrMuraliS <84494955+MrMuraliS@users.noreply.github.com>
2021-10-04 15:53:28 +0530

    Add files via upload

---
+++ Data/DataToTest.py
@@ -0,0 +1,7 @@
+TestData = [{'FirstName': 'Murali', 'LastName': 'S', 'Title': 'Soft',
+             'Email': 'Testing@testing.com', 'Phone': 1234567890,
+             'Company': 'TCS', 'NumOfEmployees': '15', 'Country': 'India',
+             'Language': 'English'}, {'FirstName': 'Rahul', 'LastName': 'S', 'Title': 'Soft',
+                                      'Email': 'Joy@joining.com', 'Phone': 9234567890,
+                                      'Company': 'TCS', 'NumOfEmployees': '15', 'Country': 'India',
+                                      'Language': 'English'}]
